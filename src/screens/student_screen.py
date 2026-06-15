9364772faa3bb1150a19623d63fe8e58b51d9546
Delta Student <krishnagupta2402@gmail.com>
2026-06-15 17:12:37 +0530

    project

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -2,6 +2,9 @@ import streamlit as st
 from src.ui.base_layout import style_background_dashboard,style_base_layout
 from src.components.header import header_dashboard
 from src.components.footer import footer_dashboard
+from PIL import Image
+import numpy as np
+
 
 def student_screen():
     style_background_dashboard()
@@ -18,5 +21,8 @@ def student_screen():
     st.space()
     st.space()
 
-    st.camera_input("Position your face in the center")
+    photo_source = st.camera_input("Position your face in the center")
+
+    if photo_source:
+        np.array(Image.open())
     footer_dashboard()
