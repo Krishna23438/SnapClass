01762e0c6d90ce85bb6d87c146cc450b795a2d1a
Delta Student <krishnagupta2402@gmail.com>
2026-06-15 17:29:44 +0530

    commit

--- src/pipelines/face_pipeline.py
+++ src/pipelines/face_pipeline.py
@@ -69,15 +69,31 @@ def train_classifier():
 def predict_attendance(class_image_np):
   encodings = get_face_embeddings(class_image_np)
 
-  detect_student = {}
+  detected_student = {}
 
   model_data = get_trained_model()
 
   if not model_data:
-    return detect_student, [], len(encodings) # we return 1.the values of students/their embeddings, 2.lists of students 3.No. of students
+    return detected_student, [], len(encodings) # we return 1.the values of students/their embeddings, 2.lists of students 3.No. of students
   
   clf = model_data['clf']
-  X = model_data['X']
+  X_train = model_data['X']
   y_train = model_data['y']
 
   all_students = sorted(list(set(y_train)))
+
+  for encoding in encodings:
+    if len(all_students>=2):
+      predicted_id = int(clf.predict([encoding])[0])
+    else:
+      predicted_id = int(all_students[0])
+
+    student_embedding = X_train[y_train.index(predicted_id)]
+
+    best_match_score = np.linalg.norm(student_embedding - encoding)
+
+    resemblance_threshold = 0.6
+
+    if best_match_score <= resemblance_threshold:
+      detected_student[predicted_id] = True
+  return detected_student, all_students, len(encoding)
