7a09fae1dc858ba9a623da790df8fbd9f71fd515
Delta Student <krishnagupta2402@gmail.com>
2026-06-20 08:19:21 +0530

    correction

--- src/database/db.py
+++ src/database/db.py
@@ -70,15 +70,15 @@ def create_subject(subject_code,name,section,teacher_id):
 
 def get_teacher_subjects(teacher_id):
    response = supabase.table('subjects').select("*,subject_students(count),attendance_logs(timestamp)").eq("teacher_id",teacher_id).execute()
-   return response.data
+   subjects = response.data
 
    for sub in subjects:
-      sub['total_students'] = sub.get("subjects_student",[{}])[0].get('count',0) if sub.get('subject_students') else 0
-      attendance = sub.get('attendance_logs',[])
-      unique_sessions = len(set(log['timestamp'] for log in attendance))
-      sub['total_classes'] = unique_sessions
+       sub['total_students'] = sub.get("subjects_student",[{}])[0].get('count',0) if sub.get('subject_students') else 0
+       attendance = sub.get('attendance_logs',[])
+       unique_sessions = len(set(log['timestamp'] for log in attendance))
+       sub['total_classes'] = unique_sessions
 
-      sub.pop('subject_student',None)
-      sub.pop('attendance_logs',None)
+       sub.pop('subject_student',None)
+       sub.pop('attendance_logs',None)
 
    return subjects
